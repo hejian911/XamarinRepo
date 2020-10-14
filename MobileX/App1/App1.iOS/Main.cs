628c97fd1b1caee1c314ec0ec9a93a5c6f91c080
hejian911 <hejian911@gmail.com>
2020-10-14 23:43:59 +1100

    add dummy login

--- MobileX/App1/App1.iOS/Main.cs
+++ MobileX/App1/App1.iOS/Main.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
-namespace App1.iOS
+namespace MobileX.iOS
 {
     public class Application
     {
--- MobileX/MobileX/MobileX/App.xaml.cs
+++ MobileX/MobileX/MobileX/App.xaml.cs
@@ -6,11 +6,16 @@ namespace MobileX
 {
     public partial class App : Application
     {
+        public static bool IsUserLoggedIn { get; set; }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+       			if (!IsUserLoggedIn) {
+				MainPage = new NavigationPage (new LoginPage ());
+			} else {
+				MainPage = new NavigationPage (new MainPage ());
+			}
         }
 
         protected override void OnStart()
